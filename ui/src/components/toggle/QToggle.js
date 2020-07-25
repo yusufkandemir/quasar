2b2d4fd2d794d291706dd2eaaf617da47c9a2433
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-07-25 11:12:18 +0300

    fix(QToggle): Temporarily enable render with a workaround

--- ui/src/components/toggle/QToggle.js
+++ ui/src/components/toggle/QToggle.js
@@ -55,8 +55,9 @@ export default defineComponent({
 
   created () {
     this.type = 'toggle'
-  }
+  },
 
   // TODO: Vue 3, render function from the mixin somehow can't make it into here
   // see: https://github.com/vuejs/vue-next/issues/1630
+  render: CheckboxMixin.render
 })
